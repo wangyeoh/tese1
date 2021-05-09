e1551169c2727930850065f149d8305dd292ad3f
wangyeoh <40331408@163.com>
2021-05-09 05:11:56 +0000

    master

--- csdn01/01.py
+++ csdn01/01.py
@@ -1,2 +1,16 @@
 测试
 测试2
+ces ter
+> git rev-parse --symbolic-full-name master@{u}
+fatal: no upstream configured for branch 'master'
+> git for-each-ref --sort -committerdate --format %(refname) %(objectname) %(*objectname)
+> git remote --verbose
+> git config --get commit.template
+> git rev-list --count 991c0f
+ter
+> git rev-parse --symbolic-full-name master@{u}
+fatal: no upstream configured for branch 'master'
+> git for-each-ref --sort -committerdate --format %(refname) %(objectname) %(*objectname)
+> git remote --verbose
+> git config --get commit.template
+> git rev-list --count 991c0f
